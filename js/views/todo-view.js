4d6bc634ba3d523dfba45ac544816192700d7e01
JSco <jackmichaelscotty@gmail.com>
2017-03-04 23:30:41 -0600

    added priority status

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -18,6 +18,7 @@ var app = app || {};
         // The DOM events specific to an item.
         events: {
             'click .toggle': 'toggleCompleted',
+            'click .priority-btn': 'togglePriority',
             'dblclick label': 'edit',
             'click .edit-btn': 'edit',
             'click .destroy': 'clear',
@@ -51,6 +52,7 @@ var app = app || {};
 
             this.$el.html(this.template(this.model.toJSON()));
             this.$el.toggleClass('completed', this.model.get('completed'));
+            this.$el.toggleClass('priority', this.model.get('priority'));
             this.toggleVisible();
             this.$input = this.$('.edit');
             return this;
@@ -71,6 +73,15 @@ var app = app || {};
             this.model.toggle();
         },
 
+        // Toggle the `"priority"` state of the model.
+        togglePriority: function () {
+            this.model.prioritize();
+        },
+
+        isPriority: function () {
+            return this.model.get('priority');
+        },
+
         // Switch this view into `"editing"` mode, displaying the input field.
         edit: function () {
             this.$el.addClass('editing');
@@ -79,8 +90,8 @@ var app = app || {};
 
         // Close the `"editing"` mode, saving changes to the todo.
         close: function () {
-            var value = this.$input.val();
-            var trimmedValue = value.trim();
+            var value = this.$input.val(),
+                trimmedValue = value.trim();
 
             // We don't want to handle blur events from an item that is no
             // longer being edited. Relying on the CSS class here has the
