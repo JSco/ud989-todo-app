c14cd4d258f2dfb7f2cf6587b2935ed3c4c05420
JSco <jackmichaelscotty@gmail.com>
2017-03-04 20:37:47 -0600

    organized syntax

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 /*global $ */
 /*jshint unused:false */
-var app = app || {};
-var ENTER_KEY = 13;
-var ESC_KEY = 27;
+var app = app || {},
+    ENTER_KEY = 13,
+    ESC_KEY = 27;
 
 $(function () {
-	'use strict';
+    'use strict';
 
-	// kick things off by creating the `App`
-	new app.AppView();
+    // kick things off by creating the `App`
+    new app.AppView();
 });
